eda0b199e72f8ded7cf39ad0df6ced5a06c30533
AfshaMohammed <mdafsha.7@gmail.com>
2017-10-15 14:02:00 -0500

    ReactJS
    Prop Validations

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -34,6 +34,42 @@ class Footer extends React.Component{
 	}
 }
 
+class Validations extends React.Component{
+	render(){
+	   return(
+		<div>   
+		    <h1>Props Validations</h1>
+			<p>Array: {this.props.propArray}</p>
+			<p>Boolean: {this.props.propBool ? "true..." : "false.."}</p>
+			<p>Number: {this.props.propNumber}</p>
+			<p>String: {this.props.propString}</p>
+			<p>Object: {this.props.propObject}</p>
+			<p>Function: {this.props.propFunc(5)}</p>
+		</div>
+	 );
+	}
+}
+Validations.proprTypes = {
+	propArray: React.PropTypes.array.isRequired,
+   propBool: React.PropTypes.bool.isRequired,
+   propFunc: React.PropTypes.func,
+   propNumber: React.PropTypes.number,
+   propString: React.PropTypes.string,
+   propObject: React.PropTypes.object
+}
+Validations.defaultProps = {
+	propArray: [1,2,3,4],
+	propBool: true,
+	propNumber: 2,
+	propString: 'Apple',
+	propFunc: function(a){return a},
+	propObject: {
+			name: 'Afsha',
+			Age: 25,
+			gender: 'female'
+	}
+}
+
 class MyComponent extends React.Component{
 	render() {
 		return(
