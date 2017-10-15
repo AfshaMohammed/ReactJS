ba0d6e1273e56c24f6a7641b7e6068f6fe34e056
AfshaMohammed <mdafsha.7@gmail.com>
2017-10-15 16:37:27 -0500

    ReactJS
    Here I use component API- setState( ) method

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -16,7 +16,7 @@ class Header extends React.Component{
 }
 
 class Footer extends React.Component{
-	constructor(props){
+constructor(props){
 	super(props)
 	this.state = {
 		firstName : 'Afsha',
@@ -24,56 +24,40 @@ class Footer extends React.Component{
 	}
 }
 	render(){
-	setTimeout(() => {this.setState({firstName: 'Raghava', lastName: 'Ammisetty'})},5000);
+		setTimeout(()=> {this.setState({firstName: 'Raghava', lastName:'Ammisetty'})},4000);
 		return(
 		<div>
 			<h1>Footer</h1>
 			<p>My full name is {this.state.firstName} {this.state.lastName}</p>
-		</div>
+			</div>
 		);
 	}
 }
 
-class Validations extends React.Component{
-	render(){
-	   return(
-		<div>   
-		    <h1>Props Validations</h1>
-			<p>Array: {this.props.propArray}</p>
-			<p>Boolean: {this.props.propBool ? "true..." : "false.."}</p>
-			<p>Number: {this.props.propNumber}</p>
-			<p>String: {this.props.propString}</p>
-			<p>Object: {this.props.propObject}</p>
-			<p>Function: {this.props.propFunc(5)}</p>
-		</div>
-	 );
-	}
-}
-Validations.proprTypes = {
-	propArray: React.PropTypes.array.isRequired,
-   propBool: React.PropTypes.bool.isRequired,
-   propFunc: React.PropTypes.func,
-   propNumber: React.PropTypes.number,
-   propString: React.PropTypes.string,
-   propObject: React.PropTypes.object
-}
-Validations.defaultProps = {
-	propArray: [1,2,3,4],
-	propBool: true,
-	propNumber: 2,
-	propString: 'Apple',
-	propFunc: function(a){return a},
-	propObject: {
-			name: 'Afsha',
-			Age: 25,
-			gender: 'female'
-	}
-}
+
 
 class MyComponent extends React.Component{
+	 constructor() {
+      super();
+		
+      this.state = {
+         data: []
+      }
+	
+      this.setStateHandler = this.setStateHandler.bind(this);
+   };
+
+   setStateHandler() {
+      var item = "setState..."
+      var myArray = this.state.data;
+      myArray.push(item)
+     
+      this.setState({data: this.state.data+item})
+   };
 	render() {
 		return(
 			<div style={styles}>
+
 				<h2 style={styles1}>Hello World!</h2>
 				<h3>My First React JS program</h3>
 				<p>Using Styles</p>
@@ -81,6 +65,9 @@ class MyComponent extends React.Component{
 				<Header name="Afsha" />
 				<Header name="Mohammed" />
 				<Footer/>
+				
+				<button onClick={this.setStateHandler}>Set State </button>
+		<p>State Array: {this.state.data}</p>
 			</div>
 		);
 	}
