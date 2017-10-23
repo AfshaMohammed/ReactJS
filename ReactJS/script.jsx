01c3995911f83fd4a4c0479a1e58d90c20081c3b
AfshaMohammed <mdafsha.7@gmail.com>
2017-10-22 22:21:04 -0500

    React JS-Forms
    React forms are similar to HTML forms. But in React, the state is contained in the state property of the component and is only updated via setState(). Thus the elements can’t directly update their state and their submission is handled by a JavaScript function. This function has full access to the data that is entered by the user into a form.

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -1,71 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class Parent extends React.Component {
 
-   constructor(props) {
-      super(props);
-		
+class Forms extends React.Component{
+   constructor(){
+      super();
       this.state = {
-         data: 0
+         data: 'Update data...!'
       }
-
-      this.setNewNumber = this.setNewNumber.bind(this)
+      this.updateState = this.updateState.bind(this);
    };
-
-   setNewNumber() {
-      this.setState({data: this.state.data + 1})
+   updateState(e){
+      this.setState({data: e.target.value});
    }
 
-   render() {
-      return (
+
+   render(){
+      return(
          <div>
-            <button onClick = {this.setNewNumber}>INCREMENT</button>
-            <Child myNumber = {this.state.data}></Child>
-         </div>
-      );
-   }
-}
-
-class Child extends React.Component {
-
-   componentWillMount() {
-      console.log('Component WILL MOUNT!')
-   }
-
-   componentDidMount() {
-      console.log('Component DID MOUNT!')
-   }
-
-   componentWillReceiveProps(newProps) {    
-      console.log('Component WILL RECIEVE PROPS!')
-   }
-
-   shouldComponentUpdate(newProps, newState) {
-      return true;
-   }
-
-   componentWillUpdate(nextProps, nextState) {
-      console.log('Component WILL UPDATE!');
-   }
-
-   componentDidUpdate(prevProps, prevState) {
-      console.log('Component DID UPDATE!')
-   }
-
-   componentWillUnmount() {
-      console.log('Component WILL UNMOUNT!')
-   }
-	
-   render() {
-      return (
-         <div>
-            <h3>{this.props.myNumber}</h3>
+            <input type="text" value={this.state.data}
+            onChange={this.updateState} />
+            <h4>{this.state.data}</h4>
          </div>
       );
    }
 }
 ReactDOM.render(
-	<Parent/>, document.getElementById('content')
+   <Forms/>, document.getElementById('content')
 );
-setTimeout(() => {
-   ReactDOM.unmountComponentAtNode(document.getElementById('content'));}, 10000);
