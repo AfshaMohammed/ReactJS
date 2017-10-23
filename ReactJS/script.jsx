3797fdb9eb98bf1a9e2bd6eb24f1c5698a611c4c
AfshaMohammed <mdafsha.7@gmail.com>
2017-10-23 17:52:26 -0500

    ReactJS-Stateful component
    When a component is stateful. It is a central point to store all information about the app/component changes occur, it has the ability to change the state. It has knowledge about past, present and potential future state changes

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-      constructor(){
-         super();
-         this.state = {
-            data: ''
-         }
-         this.updateState = this.updateState.bind(this);
-         this.clearData = this.clearData.bind(this);
-      }
 
-      updateState(e){
-         this.setState({data: e.target.value});
-      }
-      clearData(){
-         this.setState({data: ''});
-         ReactDOM.findDOMNode(this.refs.myInput).focus();
-      }
+class MyComponent extends React.Component{
+   render(){
+   return(
+      <div>
+      
+      <Text/>
+      <Input/>
+      </div>
+   );
 
+   }
+}
+class Text extends React.Component{
    render(){
       return(
-         <div>
-            <input type="text" value={this.state.data} onChange={this.updateState} ref="myInput" />
-            <button onClick={this.clearData}>Click</button>
-            <h4>{this.state.data}</h4>
-         </div>
+         <h1>It is a Stateful Component</h1>
       );
    }
 }
+class Input extends React.Component{
+      render(){
+         return(
+            <input type="text" name="someText"/>
+         );
+      }
+
+}
 ReactDOM.render(
-   <App/>, document.getElementById('content')
+   <MyComponent/>, document.getElementById('content')
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
