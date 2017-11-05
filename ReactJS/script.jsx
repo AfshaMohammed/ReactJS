872c9ba38f61d482c224ce0430751fbf1cad2939
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-05 13:49:44 -0600

    ReactJS- Props properties
    Props properties and default property

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -1,28 +1,26 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
    render(){
       return(
-            <h1>{this.props.text}</h1>
+         <div>
+
+               <h1>{this.props.text}</h1>
+         </div>
          );
    }
 }
+App.propTypes = {
+   text: React.propTypes.string,
+   cat: React.propTypes.number.isRequired
+}
+App.defaultProps = {
+   text: "This is a default text"
+}
 ReactDOM.render(
-      <App text= "Hello! Welcome..."/>, document.getElementById('content')
+      <App cat={5} text= "This is a props text" />, document.getElementById('content')
    );
-
-
-
-
-
-
-
-
-
-
-
 
 
 
