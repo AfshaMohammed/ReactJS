28f351f83752c378aef065466311384302f5c345
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-05 14:00:25 -0600

    ReactJS--State update
    State update

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
+   constructor(){
+      super();
+      this.state = {
+         text: "This is a state text"
+      }
+   }
+   update(e){
+      this.setState({text: e.target.value})
+   }
    render(){
       return(
          <div>
-
-               <h1>{this.props.text}</h1>
+               <input type="text" onChange={this.update.bind(this)} />
+               <h1>{this.state.text}</h1>
          </div>
          );
    }
 }
-App.propTypes = {
-   text: React.propTypes.string,
-   cat: React.propTypes.number.isRequired
-}
-App.defaultProps = {
-   text: "This is a default text"
-}
+
 ReactDOM.render(
-      <App cat={5} text= "This is a props text" />, document.getElementById('content')
+      <App />, document.getElementById('content')
    );
 
 
