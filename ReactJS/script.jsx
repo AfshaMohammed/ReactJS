01a7c99241fb8c00684b8b7ba4bab9a00105887d
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-09 12:52:03 -0600

    React JS
    Component to component communication

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 class App extends React.Component{
-   constructor(){
-      super();
-      this.state = {
-         text: "This is a state text!"
-      }
-   }
-   update(e){
-      this.setState({text: e.target.value})
-   }
    render(){
       return(
-         <div>
-               <input type="text" onChange={this.update.bind(this)} />
-               <h1>{this.state.text}</h1>
-         </div>
+            <button>I <Header /> React</button>
+         );
+   }
+}
+class Header extends React.Component{
+   render(){
+      return(
+            <span>&hearts;</span>
          );
    }
 }
