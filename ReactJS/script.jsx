809dc5745248874bf13f5e55f228f4c543b9589f
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-09 13:55:18 -0600

    ReactJS
    Props validations

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -2,32 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 class App extends React.Component{
    render(){
       return(
-            <button>I <Header /> React</button>
+            <Title text='1234561' />
          );
    }
 }
-class Header extends React.Component{
-   render(){
-      return(
-            <span>&hearts;</span>
-         );
-   }
-}
+const Title = (props) => <h1>Title: {props.text}</h1>
 
+Title.propTypes = {
+   text(props, propName, component){
+      if(!(propName in props)){
+         return new Error("missing ${propName}")
+      }
+      if(props[propName].length < 5 ){
+         return new Error("${propName} was too short!")
+      }
+   }
+}
 ReactDOM.render(
       <App />, document.getElementById('content')
    );
-
-
-
-
-
-
-
-
-
-
