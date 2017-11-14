1c12b5085eee0ef030d74e2f420cd519f6abd0e5
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-14 17:52:00 -0600

    ReactJS
    React JS synthetic event system

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+   
+
 
 class App extends React.Component{
    constructor(){
       super();
       this.state = {
-         data: "My data is "
+         currentEvent: "---"
       }
-
+      this.update = this.update.bind(this);
    }
    update(e){
-      this.setState({data: e.target.value})
+      this.setState({data: e.type})
    }
+
    render(){
       return(
-            <div>
-               <h1>{this.state.data}</h1>
-               <Header update={this.update.bind(this)} />
-               <Header update={this.update.bind(this)} />
-               <Header update={this.update.bind(this)} />
-               <Header update={this.update.bind(this)} />
-            </div>
-         )
+         <div><textarea onKeyPress = {this.update} onCopy = {this.update} onCut = {this.update} onPaste = {this.update} 
+         onFocus= {this.update} onBlur={this.update} col="30" row="30" />
+           <h1>{this.state.currentEvent}</h1>
+         </div>
+           ); 
    }
 }
-const Header = (props) => <input type="text" onChange={props.update} />
+
 ReactDOM.render(
       <App />, document.getElementById('content')
    );
