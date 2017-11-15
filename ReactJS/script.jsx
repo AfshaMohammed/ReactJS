500b6644df3d7a03f13143b9727981ba6957e750
AfshaMohammed <mdafsha.7@gmail.com>
2017-11-14 18:01:24 -0600

    React JS
    React synthetic events

--- ReactJS/script.jsx
+++ ReactJS/script.jsx
@@ -2,29 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
    
 
-
 class App extends React.Component{
    constructor(){
       super();
       this.state = {
-         currentEvent: "---"
+         data: '---'
       }
-      this.update = this.update.bind(this);
-   }
-   update(e){
-      this.setState({data: e.type})
-   }
+      this.update = this.update.bind(this)
+      }
+      update(e){
+         this.setState({data: e.type})
+      }
 
    render(){
       return(
-         <div><textarea onKeyPress = {this.update} onCopy = {this.update} onCut = {this.update} onPaste = {this.update} 
-         onFocus= {this.update} onBlur={this.update} col="30" row="30" />
-           <h1>{this.state.currentEvent}</h1>
-         </div>
-           ); 
+            <div>
+               <textarea onChange={this.update} onCopy={this.update} onCut={this.update} onPaste={this.update} onFocus={this.update} onDoubleClick={this.update} onBlur={this.update} col="50" row="30" />
+               <h1>{this.state.data}</h1>
+            </div>
+         );
    }
 }
-
 ReactDOM.render(
       <App />, document.getElementById('content')
    );
